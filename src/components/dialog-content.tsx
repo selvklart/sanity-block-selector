272f6e02dfea46cbf49abd51e6968d782d24a196
Bruno Santos <bruno@selvklart.no>
2024-03-15 14:50:15 +0100

    fix: fixed height of dialog

--- src/components/dialog-content.tsx
+++ src/components/dialog-content.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 import type {Group} from '../types.d';
 import {cn} from '../utils';
@@ -14,8 +14,15 @@ interface Props {
 
 export const DialogContent = ({groups, filter = ''}: Props) => {
     const [value, setValue] = useState(filter);
+    const [ref, setRef] = useState<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        const card = ref?.closest('[data-ui="Card"]') as HTMLDivElement | null;
+        card?.style.setProperty('height', '80%');
+    }, [ref]);
+
     return (
-        <div className={cn('px-8', 'pb-8', 'pt-3')}>
+        <div className={cn('px-8', 'pb-8', 'pt-3')} ref={setRef}>
             <Search value={value} onChange={setValue} />
             <SearchResults groups={groups} filter={value} />
             <Accordion groups={groups} filter={value} />
